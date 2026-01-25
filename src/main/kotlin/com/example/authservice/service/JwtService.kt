52e08e5f94686cafe2b1c3f043701d246563dbc9
Ahmadullah Mukhlis <nasarimukhlis@gmail.com>
2026-01-25 18:26:48 +0430

    the user permision is fxed

--- src/main/kotlin/com/example/authservice/service/JwtService.kt
+++ src/main/kotlin/com/example/authservice/service/JwtService.kt
@@ -1,5 +1,6 @@
 package com.example.authservice.service
 
+import com.example.authservice.entity.UserEntity
 import io.jsonwebtoken.Claims
 import io.jsonwebtoken.Jwts
 import io.jsonwebtoken.security.Keys
@@ -17,19 +18,20 @@ class JwtService {
     private val ACCESS_TOKEN_EXPIRATION = 15 * 60 * 1000
     private val REFRESH_TOKEN_EXPIRATION = 7 * 24 * 60 * 60 * 1000
 
-    fun generateAccessToken(username: String): String {
-        return createToken(username, ACCESS_TOKEN_EXPIRATION)
+    fun generateAccessToken(user: UserEntity): String {
+        return createToken(user, ACCESS_TOKEN_EXPIRATION)
     }
 
-    fun generateRefreshToken(username: String): String {
-        return createToken(username, REFRESH_TOKEN_EXPIRATION)
+    fun generateRefreshToken(user: UserEntity): String {
+        return createToken(user, REFRESH_TOKEN_EXPIRATION)
     }
 
-    private fun createToken(username: String, expirationMillis: Int): String {
+    private fun createToken(user: UserEntity, expirationMillis: Int): String {
         val now = Date()
         val expiry = Date(now.time + expirationMillis)
         return Jwts.builder()
-            .subject(username)
+            .subject(user.username)                 // store username
+            .claim("roles", user.roles)            // add roles=
             .issuedAt(now)
             .expiration(expiry)
             .signWith(secretKey)
@@ -47,6 +49,16 @@ class JwtService {
 
     fun extractUsername(token: String): String {
         return getClaims(token).subject
+    }
+
+    fun extractRoles(token: String): List<String> {
+        val roles = getClaims(token)["roles"]
+        return if (roles is List<*>) roles.filterIsInstance<String>() else emptyList()
+    }
+
+    fun extractPermissions(token: String): List<String> {
+        val permissions = getClaims(token)["permissions"]
+        return if (permissions is List<*>) permissions.filterIsInstance<String>() else emptyList()
     }
 
     private fun getClaims(token: String): Claims {
