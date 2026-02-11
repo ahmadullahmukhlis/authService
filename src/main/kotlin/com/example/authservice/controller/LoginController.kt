d77662b921ffb260aacc588b6a236f4570749a93
Ahmadullah Mukhlis <nasarimukhlis@gmail.com>
2026-02-11 18:18:12 +0430

    the problem ofhte api auth service is fixed

--- src/main/kotlin/com/example/authservice/controller/LoginController.kt
+++ src/main/kotlin/com/example/authservice/controller/LoginController.kt
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
-@RequestMapping("/api/auth")
+@RequestMapping()
 class LoginController(
     private val authService: AuthService
 ) {
