0cbc6bfc9c96244cd1793aa02a180dde0e436a51
Ahmadullah Mukhlis <nasarimukhlis@gmail.com>
2026-02-11 19:14:06 +0430

    Teh problem is fxed

--- src/main/kotlin/com/example/authservice/dto/RoleGroup/RoleRequest.kt
+++ src/main/kotlin/com/example/authservice/dto/RoleGroup/RoleRequest.kt
@@ -10,22 +10,4 @@ data class RoleRequest(
     val permissionIds: List<Long> = emptyList()
 )
 
-data class RoleResponse(
-    val id: Long?,
-    val name: String,
-    val roleGroupId: Long?,
-    val roleGroupName: String?,
-    val permissions: List<PermissionSimpleResponse> = emptyList()
-)
 
-data class PermissionSimpleResponse(val id: Long?, val name: String?)
-
-fun RoleEntity.toResponse(): RoleResponse {
-    return RoleResponse(
-        id = this.id,
-        name = this.name,
-        roleGroupId = this.roleGroup?.id,
-        roleGroupName = this.roleGroup?.name,
-        permissions = this.permissions.map { PermissionSimpleResponse(it.id, it.name) }
-    )
-}
