fdf132cfac75a681b9428623e3ca41d8036141dc
Ahmadullah Mukhlis <nasarimukhlis@gmail.com>
2026-02-15 10:40:19 +0430

    the problem of he prmisison i sfixed

--- src/main/kotlin/com/example/authservice/controller/PermissionController.kt
+++ src/main/kotlin/com/example/authservice/controller/PermissionController.kt
@@ -8,7 +8,7 @@ import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.*
 
 @RestController
-@RequestMapping("/api/v1/permissions")
+@RequestMapping("/permissions")
 class PermissionController(private val permissionService: PermissionService) {
 
     @PostMapping
